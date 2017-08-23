2dea29d0c9ad6ebd726c78bcbcae815a608b6b50
MartinPavlik <martin.pavlik100@gmail.com>
2017-08-23 18:29:33 +0200

    Fixed cors issues

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ var Links = require('./model/Links')
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next()
 })
